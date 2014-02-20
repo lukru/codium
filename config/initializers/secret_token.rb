2296e635267225cd3d80b6125915e8cec4f2bc70
Daniel T <tsui.daniel@gmail.com>
2014-02-20 12:14:30 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codium::Application.config.secret_key_base = '5b9a8b4a2968b93b3a2c4e41a475fa233e30a2928c3982dbbb3255215086be7bb4f0c9e7ddcdd00e61b79696c2bc942a9e1df013611dcd1bfe4f61e22ebf1804'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codium::Application.config.session_store :cookie_store, key: '_codium_session'
