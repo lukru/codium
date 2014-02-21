57c3641c5d2bab66e23ada54506b04249b2af4b2
Eduard Fastovski <fedd92@gmail.com>
2014-02-21 12:56:14 +1100

    recommendations works in console, fixed plurals, added kind column for later

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,13 +1,32 @@
 class RecommendationsController < ApplicationController
 
+before_action :set_post
+
+
 def create 
+	@recommendation = @post.recommendations.new(recommendation_params)
+    @recommendation.user = current_user
+    if @recommendation.save
+      redirect_to :back
+    else
+      redirect_to :back,
+      :notice => "You have already recommended this"
+    end
 end
 
 def destroy
+	@recommendation.destroy
+end
+
+def recommendation_params
+	params.require(:recommendation).permit(:kind)
 end
 
 
 
+def set_post
+	@post = Post.find(params[:post_id])
+end
 
 
 end
--- app/models/recommendations.rb
+++
@@ -1,8 +0,0 @@
-class Recommendations < ActiveRecord::Base
-
-validates :user_id, uniqueness: {scope: :post}
-
-belongs_to :user
-belongs_to :post
-
-end
