255b5b01957db70946398a0e6bd7252bd51c8ab1
albertodotcom <alberto.forn@gmail.com>
2014-02-21 15:00:01 +1100

    add get route for destroy user session

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Codium::Application.routes.draw do
 
   # Users
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   get 'users/:id' => 'users#show'
   get 'me/profile' => 'users#profile'
 
