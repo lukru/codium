a1dc81d983706ba41a79aec9cafa87e443acd63c
albertodotcom <alberto.forn@gmail.com>
2014-02-21 12:28:58 +1100

    Add allowed params for devise controller to accept user added field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:given_name, :family_name, :photo, :description, :skills, :tagline]
+    devise_parameter_sanitizer.for(:account_update) << [:given_name, :family_name, :photo, :description, :skills, :tagline]
+  end
+
 end
