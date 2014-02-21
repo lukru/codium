48700864894d5ac422ee46f342295e09e2ab1c87
lukemesiti <lukemesiti@gmail.com>
2014-02-21 16:35:48 +1100

    modify post body to be text. create functions to calculate word count and reading time

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   def word_count
+    count = Float(self.body.scan(/\w+/).size)
+  end
+
+  def reading_time
+    minute = (word_count/250).round(1)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140221004445) do
+ActiveRecord::Schema.define(version: 20140221052716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20140221004445) do
     t.datetime "updated_at"
     t.boolean  "published"
     t.string   "subtitle"
-    t.string   "body"
+    t.text     "body"
   end
 
   create_table "projects", force: true do |t|
