6e4174a0ddb7172d372ff57387f299f297f10483
albertodotcom <alberto.forn@gmail.com>
2014-02-21 14:58:03 +1100

    commented out users' validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,14 @@ class User < ActiveRecord::Base
   has_many :memberships
   has_many :projects, :through => :memberships
 
-  validates :given_name, :family_name,
-            presence: {message: 'You need to insert family name and given name'},
-            unless: :omniauth?
-
-  validates :tagline, length: {maximum: 255, message: 'Your tag line must have less then 255 characters'}
-
-  def omniauth?
-    uid && provider
-  end
+  # TODO validation
+  #validates :given_name, :family_name,
+  #          presence: {message: 'You need to insert family name and given name'},
+  #          unless: :omniauth?
+  #
+  #validates :tagline, length: {maximum: 255, message: 'Your tag line must have less then 255 characters'}
+  #
+  #def omniauth?
+  #  uid && provider
+  #end
 end
