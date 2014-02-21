d3cdc2db02f1aaabf7d189323283deee5077de48
fedetaglia <fede.tagliabue@gmail.com>
2014-02-21 11:37:14 +1100

    add published column for posts datatype boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220123307) do
+ActiveRecord::Schema.define(version: 20140221003237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140220123307) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "published"
   end
 
   create_table "projects", force: true do |t|
