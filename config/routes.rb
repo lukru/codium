ee39cd285056a53d1b2a250286a458352be2c0e0
albertodotcom <alberto.forn@gmail.com>
2014-02-23 09:24:20 +1100

    Create table comments and insert aliases for me/profile and user#show

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Codium::Application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  get 'me/profile' => 'users#profile'
-  get 'users/:display_name' => 'users#show'
+  get 'me/profile' => 'users#profile', as: :my_profile
+  get 'users/:display_name' => 'users#show', as: :user
 
   resources :posts
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,14 @@ ActiveRecord::Schema.define(version: 20140221061520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
     t.integer  "sluggable_id",              null: false
@@ -28,14 +36,6 @@ ActiveRecord::Schema.define(version: 20140221061520) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
-
-  create_table "comments", force: true do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "memberships", force: true do |t|
     t.integer  "user_id"
