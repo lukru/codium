8d85ef56040732272d4be2d935366cc87283dbf2
albertodotcom <alberto.forn@gmail.com>
2014-02-23 10:14:20 +1100

    Draft Users views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    custom_fields = %i('given_name family_name bio tagline display_name')
+    custom_fields = %i(given_name family_name bio tagline display_name)
     devise_parameter_sanitizer.for(:sign_up) << custom_fields
     devise_parameter_sanitizer.for(:account_update) << custom_fields
   end
