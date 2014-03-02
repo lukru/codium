7d0d547921556c485c93c94048c970c5f63603fc
albertodotcom <alberto.forn@gmail.com>
2014-03-03 10:37:11 +1100

    Change display_name to username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140221061520) do
+ActiveRecord::Schema.define(version: 20140302233511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(version: 20140221061520) do
     t.string   "family_name"
     t.text     "bio"
     t.string   "tagline"
-    t.string   "display_name"
+    t.string   "username"
     t.string   "slug"
   end
 
