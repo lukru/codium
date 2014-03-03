d2a8b2c3d76628b0079369f201fc495edfbfec02
Daniel T <tsui.daniel@gmail.com>
2014-03-03 12:00:07 +1100

    update post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,6 +9,28 @@ describe Post do
   end
 
   describe '#reading_time' do
+    context 'when word count is less than 250' do
+      it "returns 1" do
+        post = Post.new(title: "my first post", body: "a few words")
+        expect(post.reading_time).to eq 1      
+      end
+    end
+
+    context 'when word count is 250' do
+      it "returns 1" do
+        body = "hello " * 250
+        post = Post.new(title: "my first post", body: body)
+        expect(post.reading_time).to eq 1      
+      end
+    end
+
+    context 'when word count is 500' do
+      it "returns 2" do
+        body = "hello " * 500
+        post = Post.new(title: "my first post", body: body)
+        expect(post.reading_time).to eq 2      
+      end
+    end
 
   end
 end
