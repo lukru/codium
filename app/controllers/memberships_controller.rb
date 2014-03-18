a150849b9de0e3cccd428a7a92f4ab34a40cb2f3
fedetaglia <fede.tagliabue@gmail.com>
2014-03-18 15:03:34 +1100

    user can add and remove memberships on project edit page

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,9 +1,33 @@
 class MembershipsController < ApplicationController
-
+before_action :set_membership, only: [ :show, :edit, :update, :destroy]
 
 def create
-  binding.pry
+  @membership = Membership.create(membership_params)
+  if @membership.save
+    respond_to do |format|
+      format.js
+    end
+  end
+end
 
+def destroy
+  if @membership.destroy
+    respond_to do |format|
+      format.js
+    end 
+  end   
+end
+
+
+private
+
+def set_membership
+  @membership = Membership.find_by_id params[:id]
+end
+
+
+def membership_params
+  params.require(:membership).permit(:project_id, :member_id)
 end
 
 end
