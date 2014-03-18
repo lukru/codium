39fd15779005a1e085e87b702d4ea5210508e3ca
MollyMoile <marcus.hoile@gmail.com>
2014-03-18 15:24:27 +1100

    removed image column from projects, images will be handled by paperclip and aws

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140318032533) do
+ActiveRecord::Schema.define(version: 20140318042153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 20140318032533) do
     t.datetime "updated_at"
     t.text     "description"
     t.string   "url"
-    t.string   "image"
   end
 
   create_table "recommendations", force: true do |t|
