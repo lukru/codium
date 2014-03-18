6f5e7757b434168ee17af4687d48ea9f06d1da2d
MollyMoile <marcus.hoile@gmail.com>
2014-03-18 14:27:09 +1100

    added description, url and image to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140303004729) do
+ActiveRecord::Schema.define(version: 20140318032533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 20140303004729) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
+    t.string   "url"
+    t.string   "image"
   end
 
   create_table "recommendations", force: true do |t|
