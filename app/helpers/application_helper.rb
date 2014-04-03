a311f3997ff845e763ed8d0034ba5436fba8d7b3
Lauren Krueger <lukru489@gmail.com>
2014-04-04 09:48:59 +1100

    students' profile pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
-	def image_for_user(user)
+	def image_for_user(user,options={})
 	  if user.image.exists?
-	    image_tag(user.image.url)
+	    image_tag(user.image.url, options)
 	  else
-	    image_tag('placeholder.png')
+	    image_tag('placeholder.png', options)
 	  end
 	end
 
-	def image_for_project(project)
+	def image_for_project(project,options={})
 		if project.image.exists?
-			image_tag(project.image.url)
+			image_tag(project.image.url, options)
 		else
-			image_tag('placeholder.png')
+			image_tag('placeholder.png', options)
 		end
 	end
 end
